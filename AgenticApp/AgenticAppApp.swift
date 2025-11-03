02ae8f52b56262c684d2ee188ba1893af740a0a0
Volodymyr Kosmirak <volodymyr.kosmirak@gmail.com>
2025-11-03 20:42:19 +0200

    Init commit

---
+++ AgenticApp/AgenticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgenticAppApp.swift
+//  AgenticApp
+//
+//  Created by Volodymyr Kosmirak on 03.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AgenticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgenticApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AgenticApp
+//
+//  Created by Volodymyr Kosmirak on 03.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
